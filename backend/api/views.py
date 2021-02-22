7bbd0724a1c862a11c793fe03d92c59007bfa4f0
Mehran Tarif <mrmntf@gmail.com>
2021-02-22 11:26:51 +0330

    start permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
 from blog.models import Article
 from .serializers import ArticleSerialiser, UserSerialiser
 from django.contrib.auth.models import User
@@ -13,14 +14,15 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
 	queryset = Article.objects.all()
 	serializer_class = ArticleSerialiser
-	lookup_field = "slug"
 
 
 class UserList(ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerialiser
+	permission_classes = (IsAdminUser,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerialiser
+	permission_classes = (IsAdminUser,)
