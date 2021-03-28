2f04afa0b52266ce17e55886c9a5579cca1fbdf4
Mehran Tarif <mrmntf@gmail.com>
2021-03-28 11:15:59 +0200

    some improves

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from blog.models import Article
 from django.contrib.auth.models import User
 
-class ArticleSerialiser(serializers.ModelSerializer):
+class ArticleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Article
 		fields = "__all__"
 
 
-class UserSerialiser(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = "__all__"
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,29 +1,31 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly
-from blog.models import Article
-from .serializers import ArticleSerialiser, UserSerialiser
 from django.contrib.auth.models import User
+from blog.models import Article
+from .serializers import ArticleSerializer, UserSerializer
+from .permissions import (
+	IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly
+)
 
 # Create your views here.
 class ArticleList(ListCreateAPIView):
 	queryset = Article.objects.all()
-	serializer_class = ArticleSerialiser
+	serializer_class = ArticleSerializer
 
 
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
 	queryset = Article.objects.all()
-	serializer_class = ArticleSerialiser
+	serializer_class = ArticleSerializer
 	permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly)
 
 
 class UserList(ListCreateAPIView):
 	queryset = User.objects.all()
-	serializer_class = UserSerialiser
+	serializer_class = UserSerializer
 	permission_classes = (IsSuperUserOrStaffReadOnly,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
 	queryset = User.objects.all()
-	serializer_class = UserSerialiser
+	serializer_class = UserSerializer
 	permission_classes = (IsSuperUserOrStaffReadOnly,)
