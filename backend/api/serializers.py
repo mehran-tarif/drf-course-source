b137b86700d3443031fa7ae0197660e63f5c5a0f
Mehran Tarif <mrmntf@gmail.com>
2021-04-10 12:52:40 +0200

    28

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from blog.models import Article
 from django.contrib.auth import get_user_model
 
+class AuthorSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = get_user_model()
+		fields = ["id", "username", "first_name", "last_name"]
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+	author = AuthorSerializer()
+
 	class Meta:
 		model = Article
 		fields = "__all__"
